f222e58439d22bf239c79b12818099b40fb46113
hoon <jackalope5922@gmail.com>
2024-12-16 11:13:18 +0900

    feat

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,12 @@
-for i in range(1, 15+1)
+for i in range(1,18+1):
     if i % 3 == 0:
-        print('fizz ')
+        print('fizz')
+    elif i % 5 == 0:
+        print('buzz')
     else:
         print(i)
-print('hello')
+
+
+
+
+
